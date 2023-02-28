a208db25a085e193c5c44a1026ec38f9eff03396
Sandeep <samudsan2020@gmail.com>
2023-02-28 07:48:22 +0530

    added shared fact hidden feature

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,14 @@
 console.log("Hello JS");
+
+const btn_share = document.querySelector(".btn-open-sharefact");
+const form_fact = document.querySelector(".fact-form");
+
+btn_share.addEventListener("click", function () {
+  if (form_fact.classList.contains("hidden")) {
+    form_fact.classList.remove("hidden");
+    btn_share.textContent = "close";
+  } else {
+    form_fact.classList.add("hidden");
+    btn_share.textContent = "share a fact";
+  }
+});
